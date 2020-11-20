58c90abacf27d68e8c5ad30e8b0c8399ed27cdb9
skdw <kamil.gorzynski8@gmail.com>
2020-11-20 01:33:17 +0100

    A valuable callback

--- process.py
+++ process.py
@@ -4,7 +4,7 @@ from lem2 import DecisionTable
 
 def negate(function):
     def new_function(*args, **kwargs):
-       return ~function(*args, **kwargs)
+        return ~function(*args, **kwargs)
     return new_function
 
 
@@ -37,8 +37,9 @@ def lower_bound(df, decision, attributes):
         s = pd.Series(x != i for x in range(len(df)))
         r = same_attributes_neg_decision(pos_row)
         return sum(s & r) == 0
-    
-    any_attrs = pd.Series(any_attrs_neg_dec_exist(i) for i in range(len(positive)))
+
+    any_attrs = pd.Series(any_attrs_neg_dec_exist(i)
+                          for i in range(len(positive)))
     return positive & any_attrs
 
 
@@ -50,7 +51,7 @@ def upper_bound(df, decision, attributes):
 
 def process_subset(df, keys, subset_ids) -> set:
     dt = DecisionTable(keys)
-    records = df.to_dict(orient='records')
+    records = df[keys].to_dict(orient='records')
     for obj in records:
         dt.insertObject(obj)
 
@@ -59,7 +60,7 @@ def process_subset(df, keys, subset_ids) -> set:
     return DecisionTable.extractUsedAttributes(rules, verbose=True)
 
 
-def process_df(df_path, decision):
+def process_df(df, keys, decision):
     """Processes the data and extracts attributes
     considering the subset of ids
 
@@ -67,10 +68,6 @@ def process_df(df_path, decision):
         df_path (str): path to the dataframe
         decision: decision function
     """
-    df = pd.read_csv(df_path)
-    keys = df.keys().tolist()
-    print(df.head())
-
     # grades are not the attributes
     attributes = [k for k in keys if k.find('G')]
 
@@ -82,8 +79,10 @@ def process_df(df_path, decision):
     ub_subset_ids = positive_indices(ub)
 
     print('\nProcessing the lower bound subset')
+    print('(which attributes come with good results)')
     lset = process_subset(df, keys, lb_subset_ids)
     print('\nProcessing the upper bound complement subset')
+    print('(which attributes come with bad results)')
     uset = process_subset(df, keys, ub_subset_ids)
 
     lst = [lset, uset]
@@ -92,7 +91,6 @@ def process_df(df_path, decision):
 
 
 if __name__ == "__main__":
-    df_path = 'dataset/student-mat.csv'
 
     def decision(df):
         """decision - is the final grade over 10?
@@ -100,6 +98,22 @@ if __name__ == "__main__":
         """
         return df['G3'] > 10
 
-    res = process_df(df_path, decision)
+    df_path = 'dataset/student-mat.csv'
+    df = pd.read_csv(df_path)
+    keys = df.keys().tolist()
+    print(df.head())
+
+    # Limit the number of rows for processing
+    df = df.iloc[:200]
+    print(f'Number of students: {len(df)}')
+
+    # Subset of keys to be checked by the algorithm
+    sub_keys = keys[19:23]
+    print(f'Subset of keys considered:\n{sub_keys}')
+
+    s = sum(decision(df))
+    print(f'Number of students who achieved a good result: {s}')
+
+    res = process_df(df, sub_keys, decision)
     print('\nUnion of attributes:')
     print(res)
